344fd8d2548945c88bc42355615338f5c4613e4f
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-09-27 09:09:57 -0300

    Create t00.py

---
+++ t00.py
@@ -0,0 +1,11 @@
+#proto calculadora
+
+#def suma(x,y):
+ #   z = x + y
+#    return print(f"el resultado es {z}")
+#def resta(x,y):
+ #   z=x-y
+#    return print(f"el resultado es {z}")
+
+print(71// 2)
+print(70 % 2)
